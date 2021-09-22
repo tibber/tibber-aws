acd4ee221e4cda867abbdfbd6b52831a4112b72c
Mikael Örn <mikael.orn@tibber.com>
2021-09-22 12:48:52 +0200

    added endpoint to enable localstack

--- src/configure.ts
+++ src/configure.ts
@@ -2,9 +2,11 @@ import AWS from 'aws-sdk';
 
 type Configure = {
   (args: {region: string}): void;
+  (args: {endpoint: string}): void;
 };
 
-export const configure: Configure = ({region}) => {
+export const configure: Configure = ({region, endpoint}) => {
   AWS.config.region = region;
+  AWS.config.endpoint = endpoint;
   process.env.AWS_REGION = region;
 };
