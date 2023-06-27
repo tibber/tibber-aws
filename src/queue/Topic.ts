867db76a95ca2c9536b5dee8df27f0b274fc90ea
Husqvik <husqvik@hotmail.com>
2023-06-27 15:17:58 +0200

    any instead of unknown

--- src/queue/Topic.ts
+++ src/queue/Topic.ts
@@ -27,7 +27,7 @@ export class Topic {
     return new Topic(topicResponse.TopicArn, topicName, subjectName, endpoint);
   }
 
-  async push(evt: unknown, subject?: string, messageAttributes?: unknown) {
+  async push(evt: unknown, subject?: string, messageAttributes?: any) {
     const payload = {
       Message: JSON.stringify(evt),
       MessageAttributes: messageAttributes,
