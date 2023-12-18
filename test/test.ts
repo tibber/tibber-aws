c7d94f9a18f5c05c48f299085c311ae7d3f4206a
Finn Michael Halvorsen <fimmi@tibber.com>
2023-12-18 08:59:34 +0100

    fix: fix handling of s3 as stream

--- test/test.ts
+++ test/test.ts
@@ -15,7 +15,7 @@ beforeAll(async () => {
   configure({region: 'eu-west-1'});
 });
 
-it('should be able to assign several topics to buildererrr', () => {
+it('should be able to assign several topics to builderer', () => {
   const builder = new QueueSubjectListenerBuilder(
     'test-queueName',
     null,
